288b5cb6a5bbe9d42eef0bebe295efb23bed985d
Fernanda Nunes Dutra <fernandanunesdutra96@gmail.com>
2019-11-24 16:00:26 -0300

    Retornando primeiro e segundo lugar da disputa

--- src/helper/array-helper.js
+++ src/helper/array-helper.js
@@ -2,12 +2,25 @@ import StringHelper from "./string-helper";
 
 export default class ArrayHelper {
 
-    constructor(arr){
+    constructor(arr) {
         this.arr = arr;
     }
 
-    sortByStringProperty(property){
+    sortByStringProperty(property) {
         return this.arr.sort((a, b) => StringHelper.compare(a[property], b[property]));
+    }
+
+    remove(index) {
+        this.arr.splice(index, 1);
+    }
+
+    removeLessValuePropertyBetween(firstIndex, secondIndex, property) {
+
+        const removeIndex = this.arr[firstIndex][property] > this.arr[secondIndex][property] ? secondIndex : firstIndex;
+
+        this.remove(removeIndex);
+
+        return this.arr;
     }
 
 }
--- src/service/movie-service.js
+++ src/service/movie-service.js
@@ -9,11 +9,67 @@ export default class MovieService {
     }
 
     static async getChampions(selectedMovies) {
-        selectedMovies = await this.getAll();
-        const arrayHelper = new ArrayHelper(selectedMovies);
 
-        const orderedMovies = arrayHelper.sortByStringProperty(TITLE_PROPERTY);
+        selectedMovies = (await this.getAll()).slice(8);
 
-        return orderedMovies;
+        return this.compareMovies(selectedMovies);
     }
+
+    static sortMoviesByTitle(movies) {
+        const arrayHelper = new ArrayHelper(movies);
+        return arrayHelper.sortByStringProperty(TITLE_PROPERTY);
+    }
+
+    static buildMovieTreeDiagram(movies) {
+
+        const orderedMovies = this.sortMoviesByTitle(movies);
+
+        const length = orderedMovies.length;
+        const halfMovies = orderedMovies.slice(0, length / 2);
+
+        return halfMovies.reduce((arr, movie, index) => {
+            arr.push(...[movie, orderedMovies[length - (index + 1)]]);
+            return arr;
+        }, []);
+    }
+
+    static async compareMovies(movies) {
+
+        const movieTreeDiagram = this.buildMovieTreeDiagram(movies);
+
+        return this.compareRatingMovie(movieTreeDiagram, 0);
+    }
+
+    static compareRatingMovie(movies, index) {
+
+        if (this.isEndOfCompetition(movies)) {
+            return this.getClassification(movies[0], movies[1]);           
+        }
+
+        const arrayHelper = new ArrayHelper(movies);
+        movies = arrayHelper.removeLessValuePropertyBetween(index, index + 1, "nota");
+
+        return this.compareRatingMovie(movies, this.getNextMovieIndex(index, movies.length))
+    }
+
+    static getClassification(firstMovie, secondMovie){
+        const isFirstHighest = firstMovie.nota > secondMovie.nota;
+        
+        return {
+            first: isFirstHighest ? firstMovie : secondMovie,
+            second: isFirstHighest ? secondMovie : firstMovie,
+        }
+    }
+
+    static getNextMovieIndex(index, movieLength) {
+        const NEXT_GROUP = index + 1;
+        const NEXT_FASE = 0;
+
+        return index + 1 == movieLength ? NEXT_FASE : NEXT_GROUP;
+    }
+
+    static isEndOfCompetition(movies) {
+        return movies.length == 2;
+    }
+
 }
