d046f608f65bfeaeb9fd121f4ebb0460436e2a73
Fernanda Nunes Dutra <fernandanunesdutra96@gmail.com>
2019-11-24 18:39:42 -0300

    Ajustes

--- src/routers/movie-router.js
+++ src/routers/movie-router.js
@@ -3,15 +3,14 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/all", async function (req, res, next) {
+router.get("/all", async function (req, res) {
     const movies = await MovieController.getAll();
     res.json(movies);
 });
 
-router.post("/champions", async function (req, res, next) {   
+router.post("/champions", async function (req, res) {   
         
-    const service = new MovieController(req.body.selectedMovies);    
-    
+    const service = new MovieController(req.body.selectedMovies);
     const movies = await service.getChampions();
     
     res.json(movies);
